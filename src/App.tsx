c7751413a6c84e776abd523b446d4cbacedc02ff
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-05-23 21:51:40 +0200

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import './App.css';
-import {Cars} from "./Cars";
+import {MouseEvent} from 'react';
+
+
+/*const topCars = [
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mercedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'},
+]*/
 
 function App() {
 
-    const topCars = [
-        {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'},
-        {manufacturer: 'Audi', model: 'rs6'},
-    ]
+    /*const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+        console.log("Hello, Im DIMA")
+    }
+
+    const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+        console.log("Hello, Im IVAN")
+    }*/
+
+    /*const onClickHandler = (name: string) => {
+        console.log(name)
+    }*/
+
+    const foo1 = () => {
+        console.log(100200)
+    }
+
+    const foo2 = (figure: number) => {
+        console.log(figure)
+    }
 
     return (
         <div>
-            <Cars tasks={topCars}/>
+            {/*<button onClick={(event)=>{console.log("Hello")}}>MyYoutubeChanel-1</button>*/}
+            {/*<button onClick={myFirstSubscriber}>MyYoutubeChanel-1</button>*/}
+            {/*<button onClick={() => onClickHandler('Dima')}>MyYoutubeChanel-1</button>
+            <button onClick={() => onClickHandler('Ivan')}>MyYoutubeChanel-2</button>*/}
+
+            <button onClick={foo1}>1</button>
+            <button onClick={() => foo2(100200)}>2</button>
         </div>
     );
 }
