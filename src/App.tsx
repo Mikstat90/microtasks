a23ee8c0f91c1f21c0d96a3daedc630f728d6333
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-05-24 11:46:37 +0200

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {MouseEvent} from 'react';
 import {Button} from "./components/Button";
@@ -24,7 +24,7 @@ function App() {
         console.log(name)
     }*/
 
-    const Button1Foo = (subscriber: string, age: number) => {
+    /*const Button1Foo = (subscriber: string, age: number) => {
         console.log(subscriber, age)
     }
 
@@ -34,6 +34,14 @@ function App() {
 
     const Button3Foo = () => {
         console.log('Im stupid button')
+    }*/
+
+    //let a = 1
+    let[a, setA] = useState(1)
+
+    const onClickHandler = () => {
+        setA(++a)
+        console.log(a)
     }
 
     return (
@@ -43,9 +51,12 @@ function App() {
             {/*<button onClick={() => onClickHandler('Dima')}>MyYoutubeChanel-1</button>
             <button onClick={() => onClickHandler('Ivan')}>MyYoutubeChanel-2</button>*/}
 
-            <Button name={'YouTubeChanel-1'} callBack={() => Button1Foo('Im Dima', 33)}/>
+            {/*<Button name={'YouTubeChanel-1'} callBack={() => Button1Foo('Im Dima', 33)}/>
             <Button name={'YouTubeChanel-2'} callBack={() => Button2Foo('Im Mira', 6)}/>
-            <Button name={'YouTubeChanel-3'} callBack={Button3Foo}/>
+            <Button name={'YouTubeChanel-3'} callBack={Button3Foo}/>*/}
+
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
         </div>
     );
 }
