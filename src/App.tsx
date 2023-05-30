e7e6af37a259f99bf23cffc851e7fc7783b9946e
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-05-30 16:51:41 +0200

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -37,9 +37,9 @@ function App() {
     }*/
 
     //let a = 1
-    let[a, setA] = useState(1)
+    /*let[a, setA] = useState(1)
 
-    const onClickHandler = () => {
+    const onClickHandler1 = () => {
         setA(++a)
         console.log(a)
     }
@@ -47,10 +47,33 @@ function App() {
     const onClickHandler2 = () => {
         setA(0)
         console.log(a)
+    }*/
+
+    const [money, setMoney] = useState([
+        {banknote: 'dollar', nominal: 100, number: 'a1234567890'},
+        {banknote: 'dollar', nominal: 50, number: 'z1234567890'},
+        {banknote: 'ruble', nominal: 100, number: 'w1234567890'},
+        {banknote: 'dollar', nominal: 100, number: 'e1234567890'},
+        {banknote: 'dollar', nominal: 50, number: 'c1234567890'},
+        {banknote: 'ruble', nominal: 100, number: 'r1234567890'},
+        {banknote: 'dollar', nominal: 50, number: 'x1234567890'},
+        {banknote: 'ruble', nominal: 50, number: 'v1234567890'},
+    ])
+
+    let currentMoney = money
+
+    const onClickFilterHandler = (name: string) => {
+        if (name==='dollar') {
+            currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'dollar')
+        } else if (name==='ruble') {
+            currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'ruble')
+        } else if (name==='all') {
+            currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'all')
+        }
     }
 
     return (
-        <div>
+        <>
             {/*<button onClick={(event)=>{console.log("Hello")}}>MyYoutubeChanel-1</button>*/}
             {/*<button onClick={myFirstSubscriber}>MyYoutubeChanel-1</button>*/}
             {/*<button onClick={() => onClickHandler('Dima')}>MyYoutubeChanel-1</button>
@@ -60,10 +83,27 @@ function App() {
             <Button name={'YouTubeChanel-2'} callBack={() => Button2Foo('Im Mira', 6)}/>
             <Button name={'YouTubeChanel-3'} callBack={Button3Foo}/>*/}
 
-            <h1>{a}</h1>
-            <button onClick={onClickHandler}>number</button>
-            <button onClick={onClickHandler2}>0</button>
-        </div>
+            {/*<h1>{a}</h1>
+            <button onClick={onClickHandler1}>number</button>
+            <button onClick={onClickHandler2}>0</button>*/}
+            <ul>
+                {currentMoney.map((objectFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objectFromMoneyArr.banknote}</span>
+                            <span> {objectFromMoneyArr.nominal}</span>
+                            <span> {objectFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <div style={{marginLeft: '50px'}}>
+                <button onClick={() => onClickFilterHandler('all')}>all</button>
+                <button onClick={() => onClickFilterHandler('ruble')}>ruble</button>
+                <button onClick={() => onClickFilterHandler('dollar')}>dollar</button>
+            </div>
+        </>
     );
 }
 
